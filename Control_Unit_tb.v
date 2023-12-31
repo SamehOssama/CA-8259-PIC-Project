664bcc87b812b64b7bb3cc80cf3c331e833484e0
Mazen Saaed <80298280+MazenSaaed11@users.noreply.github.com>
2023-12-31 16:41:28 +0200

    Update Control_Unit_tb.v
    Added testbench for ICW control unit

--- Control_Unit_tb.v
+++ Control_Unit_tb.v
@@ -3,7 +3,7 @@ module Control_Unit_tb;
     // INPUTS
     reg WR_ENABLE_internal;
     reg RD_ENABLE_internal;		// from read-write logic
-    reg [7:0] DATA_internal;	// data bus
+    wire [7:0] DATA_internal;	// data bus
     reg A0_internal;           
     reg INTERNAL_INT_internal;  
     reg INTA__internal;			// ack from processor
@@ -12,23 +12,26 @@ module Control_Unit_tb;
     reg [2:0] IR_NUM_tb;
 
     // OUTPUTS
-    reg [2:0] CAS_OUT_tb;		//  for master
-    reg [7:0] interrupt_mask_tb;
-    reg INT_tb;
-    reg AEOI_tb;  
-    reg  [1:0] INTA_COUNT_tb;
-    reg R_internal;				// 0 >> fully-nested
-    reg sngl_internal;			// 0 >> cascaded  
-    reg LEVEL_internal;
-    reg RIRR_tb;
-    reg RISR_tb;
-    
+    wire [2:0] CAS_OUT_tb;		//  for master
+    wire [7:0] interrupt_mask_tb;
+    wire INT_tb;
+    wire AEOI_tb;  
+    wire [1:0] INTA_COUNT_tb;
+    wire R_internal;				// 0 >> fully-nested
+    wire sngl_internal;			// 0 >> cascaded  // OUTPUT?  
+    wire LEVEL_internal;
+    wire RIRR_tb;
+    wire RISR_tb;
+
+    //INTERNAL SIGNAL
+    reg [7:0]DATA_internal_REG;
+    assign DATA_internal = (WR_ENABLE_internal) ? DATA_internal_REG : DATA_internal;
     
     
     // Registers for all the wires to use in begin block
     // use anything with the suffix _tb
     
-    wire RD_ENABLE_tb;   // from read-write logic
+    /*wire RD_ENABLE_tb;   // from read-write logic
     wire WR_ENABLE_tb;
     wire [7:0] DATA_tb;   // data bus
     wire A0_tb;           
@@ -38,7 +41,7 @@ module Control_Unit_tb;
     wire R_tb;  // 0 >> fully-nested
     wire sngl_tb; // 0 >> cascaded  
     wire LEVEL_tb;
-    
+    */
     
 /* 	assign INTERNAL_INT_internal = INTERNAL_INT_tb
     assign WR_ENABLE_internal = WR_ENABLE_tb
@@ -78,16 +81,32 @@ module Control_Unit_tb;
     // Test stimulus
     initial begin
         // Initialize inputs
-        
-
-        // Test Case 1: 
-
+        #0 WR_ENABLE_internal = 1'b0;
+        #0 RD_ENABLE_internal = 1'b0;
+        #0 A0_internal = 1'b1;
+        #0 DATA_internal_REG = 8'bzzzzzzzz;
+        // Test Case 1:
+        #0 A0_internal = 1'b0;
+        #0 DATA_internal_REG = 8'b00010001;
+        #5 WR_ENABLE_internal = 1'b1;
+        #5 WR_ENABLE_internal = 1'b0;
         // Test Case 2: 
-        
+        #0 A0_internal = 1'b1;
+        #0 DATA_internal_REG = 8'b11111000;
+        #5 WR_ENABLE_internal = 1'b1;
+        #5 WR_ENABLE_internal = 1'b0;
+        #0 A0_internal = 1'b0;
         // Test Case 3: 
-
+        #0 A0_internal = 1'b1;
+        #0 DATA_internal_REG = 8'b00111100;
+        #5 WR_ENABLE_internal = 1'b1;
+        #5 WR_ENABLE_internal = 1'b0;
         //Test Case 4: 
-        
+        #0 A0_internal = 1'b1;
+        #0 DATA_internal_REG = 8'b00100001;
+        #5 WR_ENABLE_internal = 1'b1;
+        #5 WR_ENABLE_internal = 1'b0;
+
     end
 
 
